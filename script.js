888c08cc0b208986b757d3a433ee89856a287616
iliastzanis <32645619+iliastzanis@users.noreply.github.com>
2024-08-23 01:49:07 +0300

    Added user interaction in order to change the grid based on user input

--- script.js
+++ script.js
@@ -1,15 +1,46 @@
 const container = document.getElementById("container");
-const rows = 16;
-const columns = 16;
+const userInput = document.getElementById("user-input");
+const submitBtn = document.getElementById("submit-btn");
+const output = document.getElementById("output");
+let gridSize = 16;
 
-function makeGrid(rows, columns) {
-    for (let i = 0; i < rows; i++) {
-        for (let j = 0; j < columns; j++) {
-            const gridItem = document.createElement("div");
-            gridItem.classList.add("grid-item");
-            container.appendChild(gridItem);
+submitBtn.addEventListener('click', function () {
+    const input = userInput.value.trim(); // Trim user input
+    const checkUserInput = Math.floor(input); // make it Int
+    // Check if the input is empty, bigger than 100 or less that 1 and NaN
+    if (checkUserInput === "") {
+        output.textContent = "The input is empty. Please enter a number.";
+    } else if (isNaN(checkUserInput)) {
+        // Check if the input is not a valid number
+        output.textContent = "Please enter a valid number.";
+    } else if (Number(checkUserInput) > 100 || Number(checkUserInput) <= 0) {
+        output.textContent = "Please enter a number between 1 and 100.";
+    } else {
+        // If valid, display the number
+        output.textContent = `You entered: ${Number(checkUserInput)}`;
+        gridSize = checkUserInput;
+        // Clear the previous grid
+        while (container.firstChild) {
+            container.removeChild(container.firstChild);
         }
+        // Generate the new grid
+        makeGrid(gridSize);
+    }
+});
+
+//function that makes the grid and calculates it to fit on a 400x400 container
+function makeGrid(gridSize) {
+    const gridItemSize = 600 / gridSize;
+    for (let i = 0; i < gridSize * gridSize; i++) {
+
+        const gridItem = document.createElement("div");
+        gridItem.classList.add("grid-item");
+        gridItem.style.width = `${gridItemSize}px`;
+        gridItem.style.height = `${gridItemSize}px`;
+        container.appendChild(gridItem);
+
     }
 }
 
-makeGrid(rows, columns);
+//initial call of function with gridsize 16x16
+makeGrid(gridSize);
